dd3969eff2afabd16a92b699effa200b481c7674
Thanaporn1045 <78547225+Thanaporn1045@users.noreply.github.com>
2021-12-16 12:38:28 +0700

    Add files via upload

---
+++ สอบปลายภาค/final1/final1/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+
+namespace final1
+{
+    class Node
+    {
+        public string SkillName;
+        public Node right;
+        public Node left;
+        public Node Naxt;
+        public Node(string Input)
+        { 
+            SkillName = Input;
+            right = null;
+            left = null;
+            Naxt = null;
+        }
+    }
+    class Stack
+    {
+        private Node root;
+        public Stack()
+        {
+            root = null;
+        }
+
+        public void Push(Node node)
+        {
+            if (isrootnull()) { root = node; }
+            else
+            {
+                node.Naxt = root;
+                root = node;
+            }
+        }
+        public Node Pop()
+        {
+            
+            Node node = root; 
+            root = root.Naxt;
+            
+            node.Naxt = null;
+            return node;
+        }
+        public bool isrootnull()
+        { return root is null; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node root = null; Stack stack = new Stack(); int skillup = 0;
+            InputSkillName(ref root,ref stack);
+        }
+        static void InputSkillName(ref Node node, ref Stack stack)
+        {
+            char ans; 
+            Console.Write("Input skill name: ");
+            string skillname = Console.ReadLine();
+            node = new Node(skillname);
+            stack.Push(node);
+            
+            if(skillname == "?")
+            {
+
+            }
+            else 
+            { 
+                Console.Write("Add dependency for {0} ? (Y/N): ", skillname);
+                ans = char.Parse(Console.ReadLine());
+                if(ans=='Y')
+                {
+                    InputSkillName(ref node.left,ref stack);
+
+                }
+                else 
+                {
+                    
+                    stack.Pop();
+                    while(true)
+                    {
+                        
+                        Node nodecheck = stack.Pop(); if(nodecheck == null) {  break; }
+                        Console.Write("Add dependency for {0} ? (Y/N): ", nodecheck.SkillName);
+                        ans = char.Parse(Console.ReadLine()); 
+                        if(ans == 'Y') { InputSkillName(ref node.right, ref stack); break; }
+                    } 
+                    InputSkillName(ref node, ref stack);
+
+                }
+            }
+        }
+        
+    }
+}
---
+++ สอบปลายภาค/final2/final2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace final2
+{
+    class Node
+    {
+        public char Parts;
+        public Node Naxt;
+        public Node(char parts)
+        {
+            Parts = parts;
+        }
+    }
+    class Queue
+    {
+        private Node root;
+        public void Push(Node node)
+        {
+            if (root == null) { root = node; }
+            else
+            {
+                Node ptr = root;
+                while (ptr.Naxt != null)
+                {
+                    ptr = ptr.Naxt;
+                }
+                ptr.Naxt = node;
+            }
+        }
+        public Node Pop()
+        {
+            if (root == null) { return null; }
+            Node node = root;
+            root = root.Naxt;
+            node.Naxt = null;
+            return node;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue assemblylinequeue = new Queue(); Node node; int ProductCount = 0;
+            int numproduct = int.Parse(Console.ReadLine());
+            while (true)
+            { 
+                char parts = char.Parse(Console.ReadLine());
+                node = new Node(parts);
+                assemblylinequeue.Push(node);
+                if (parts == 'E') { ProductCount++; break; }
+                else
+                {
+
+                }
+            } 
+        }
+    }
+}
